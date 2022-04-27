16f9b141c808753885e77091a20b82f423646383
Saifeddine Mahjoub <saifeddine.mahjoub@outlook.com>
2022-04-27 19:45:26 +0200

    bug fixes

--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ class Project {
 }
 
 // Project State Management
-type Listener = (items: Project[]) => void
+type Listener = (items: Project[]) => void;
 class ProjectState {
   private listeners: Listener[] = [];
   private projects: Project[] = [];
@@ -135,7 +135,12 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListeners((projects: Project[]) => {
-      this.assignedProjects = projects;
+      this.assignedProjects = projects.filter((prj) => {
+        if (this.type === "active") {
+          return prj.status === ProjectStatus.Active;
+        }
+        return prj.status === ProjectStatus.Finished;
+      });
       this.renderProjects();
     });
 
@@ -147,6 +152,7 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-projects-list`
     ) as HTMLUListElement;
+    listEl.innerHTML = "";
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement("li");
       listItem.textContent = prjItem.title;
